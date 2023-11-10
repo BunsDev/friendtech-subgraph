27ecd5265f76b8913f4303b6efbfff78df6d8970
Jacek <jacek@jtrocinski.com>
2023-11-10 09:35:12 +0100

    Remove subject field from entity.

--- friendtech/src/helpers.ts
+++ friendtech/src/helpers.ts
@@ -5,7 +5,6 @@ export function createOrLoadSubject(address: Bytes): Subject {
   let subject = Subject.load(address);
   if (subject == null) {
     subject = new Subject(address);
-    subject.subject = address;
     subject.earnedFees = BigInt.fromI32(0);
     subject.trades = BigInt.fromI32(0);
     subject.tradedShares = BigInt.fromI32(0);
