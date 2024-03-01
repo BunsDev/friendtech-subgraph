d2f924bf45e9846a3e3ce97502340417bee72301
Buns Enchantress <68980965+BunsDev@users.noreply.github.com>
2024-03-01 05:28:07 -0700

    +: battle-arena (undeployed)

---
+++ battle-arena/mappings/helpers.ts
@@ -0,0 +1,14 @@
+import { BigInt, Bytes } from '@graphprotocol/graph-ts';
+import { Subject } from '../generated/schema';
+
+export function createOrLoadSubject(address: Bytes): Subject {
+  let subject = Subject.load(address);
+  if (subject == null) {
+    subject = new Subject(address);
+    subject.earnedFees = BigInt.fromI32(0);
+    subject.trades = BigInt.fromI32(0);
+    subject.tradedShares = BigInt.fromI32(0);
+  }
+
+  return subject as Subject;
+}
